ed8e2aaeaad8f2f7ad0cbab9bd02dd77e8b214a4
VictoriaYotka <115709692+VictoriaYotka@users.noreply.github.com>
2023-04-07 21:07:57 +0000

    Deploying to gh-pages from @ VictoriaYotka/victory.s-12@a5254e0c36e4c032aa0e610ce11783305d80401e 🚀

---
+++ index.cf7f3a85.js
@@ -0,0 +1,2 @@
+!function(){var o=this,n=$(".back-to-top");$(window).on("scroll",(function(){$(o).scrollTop()>=200?n.fadeIn():n.fadeOut()})),n.on("click",(function(o){o.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}();
+//# sourceMappingURL=index.cf7f3a85.js.map
---
+++ index.e9b882c4.js
@@ -0,0 +1,2 @@
+!function(){let o=$(".back-to-top");$(window).on("scroll",(()=>{$(this).scrollTop()>=200?o.fadeIn():o.fadeOut()})),o.on("click",(o=>{o.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}();
+//# sourceMappingURL=index.e9b882c4.js.map
